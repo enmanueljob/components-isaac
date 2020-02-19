27ebc781d6588bb17a5b942ecac9902f95710d2f
Enmanuel jarquin <enmisac@gmail.com>
2020-02-19 15:32:15 -0600

    se agrego configuracion inicial y el componente Button

---
+++ src/Button/Button.stories.js
@@ -0,0 +1,31 @@
+// @flow
+import React from "react"
+import { action } from "@storybook/addon-actions"
+
+import Button from "."
+
+export default {
+  title: "Button",
+  component: Button,
+  parameters: {
+    info: {
+      inline: true
+    }
+  }
+}
+
+export const defaultButton = () => (
+  <Button onClick={action("button-click")}>Click aquí</Button>
+)
+
+export const mediumButton = () => (
+  <Button onClick={action("button-click")} size="medium">
+    Click aquí
+  </Button>
+)
+
+export const underlineButton = () => (
+  <Button onClick={action("button-click")} size="medium" underline>
+    Click aquí
+  </Button>
+)
